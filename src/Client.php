00c8e31c74f5074980b24aa912918917fac8e2ce
Alexander Bretschneider <alexander.bretschneider@marini.systems>
2026-07-27 17:08:29 +0200

    Add condition parameter for containerWait

--- src/Client.php
+++ src/Client.php
@@ -807,16 +807,18 @@ class Client
      * Block until container id stops, then returns the exit code
      *
      * @param string $container container ID
+     * @param string $condition Wait until a container state reaches the given condition
      * @return PromiseInterface Promise<array> `array('StatusCode' => 0)` (see link)
      * @link https://docs.docker.com/engine/api/v1.40/#operation/ContainerWait
      */
-    public function containerWait($container)
+    public function containerWait($container, $condition = 'not-running')
     {
         return $this->browser->post(
             $this->uri->expand(
-                'containers/{container}/wait',
+                'containers/{container}/wait{?condition}',
                 array(
-                    'container' => $container
+                    'container' => $container,
+                    'condition' => $condition,
                 )
             )
         )->then(array($this->parser, 'expectJson'));
